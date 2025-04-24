3913cd4e58f405871e42da14658b55923b5ba80c
ThuySeven23 <huuthuytrann3004@gmail.com>
2025-04-24 19:38:16 +0700

    Setup VueJS

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div id="app">
+    <h1>Welcome to Your Vue.js App</h1>
+    <p>This is a simple Vue.js application.</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
